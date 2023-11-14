95b2c4cee4b4cd8aadd2f7afa2714c6cce4c64c9
Antoine Miné <antoine.mine@lip6.fr>
2023-11-14 16:15:01 +0100

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module pobj.pinboard {
+	exports pobj.pinboard.editor;
+	exports pobj.pinboard.test;
+	requires javafx.base;
+	requires javafx.graphics;
+	requires transitive javafx.controls;
+	requires junit;
+}
