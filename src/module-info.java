d7f7dd9b6db613f98c3d3fa7d3c713e09265327b
Toufic Batache <taffou2a@gmail.com>
2023-12-04 16:45:14 +0100

    Restore old module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,8 @@
 module pobj.pinboard {
-	exports pobj.pinboard.document;
 	exports pobj.pinboard.editor;
-	exports pobj.pinboard.editor.test;
-	exports pobj.pinboard.editor.tools.test;
 	exports pobj.pinboard.test;
 	requires javafx.base;
 	requires javafx.graphics;
-	requires javafx.controls;
-    requires junit4;
+	requires transitive javafx.controls;
+	requires junit;
 }
