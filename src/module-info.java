d1fea205cd0c70daac2557f2b2335458d206499a
Toufic Batache <taffou2a@gmail.com>
2024-11-05 10:55:50 +0100

    first commit

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module pobj.pinboard {
-	exports pobj.pinboard.editor;
-	exports pobj.pinboard.test;
-	requires javafx.base;
-	requires javafx.graphics;
-	requires transitive javafx.controls;
-	requires junit;
-}
